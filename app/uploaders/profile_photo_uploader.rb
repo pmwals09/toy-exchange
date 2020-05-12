93c6eb1d7710d769b69ae154bb7ad0a325f38863
Pat Walsh <pmwals09@gmail.com>
2020-05-11 22:57:31 -0400

    Added correct profile photo default links to S3 resources

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -16,12 +16,13 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url(*)
+  #   # # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   #
+  #   # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  #   "https://toy-exchange-development.s3.amazonaws.com/defaults/default.jpg"
   # end
 
   # Process files as they are uploaded:
@@ -33,10 +34,16 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :avatar do
     process resize_to_fit: [64, 64]
+    def default_url
+      "https://toy-exchange-development.s3.amazonaws.com/defaults/avatar_default.jpg"
+    end
   end
 
   version :profile do
     process resize_to_fit: [256, 340]
+    def default_url
+      "https://toy-exchange-development.s3.amazonaws.com/defaults/profile_default.jpg"
+    end
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
